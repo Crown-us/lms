d0f97770be868d9eff3efaa9e87243a78148e6e2
lenov <ww@g.c>
2025-09-22 21:23:51 +0700

    update malam 22 sep 2025

--- app/admin/courses/_components/AdminCourseCard.tsx
+++ app/admin/courses/_components/AdminCourseCard.tsx
@@ -1,16 +1,22 @@
 import {Card} from "@/components/ui/card";
 import Image from "next/image";
 import {AdminCourseType} from "@/app/data/admin/admin-get-courses";
+import {useConstructUrl} from "@/hooks/use-construct";
+
 
 interface iAppProps {
     data: AdminCourseType,
 }
 
 export function AdminCourseCard({data}: iAppProps) {
+    const thumbnailUrl = useConstructUrl(data.fileKey)
     return (
         <Card className="group relative">
             <div></div>
-            <Image src={data.fileKey} alt="Thumbnail" />
+            <Image src={thumbnailUrl} alt="Thumbnail" width={600} height={400}
+            className="w-full rounded-t-lg aspect-video h-full object-cover " />
+
+
         </Card>
     )
 }
